f749769e807960d5f6fc1fd626195885c164245b
Denis F <densib@gmail.com>
2022-03-27 01:14:55 +0300

    initial commit

---
+++ config/init.php
@@ -0,0 +1,17 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/services');
+define("HELPERS", ROOT . '/services/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'shop');
+define("PATH", 'http://mvc-shop');
+define("ADMIN", 'http://mvc-shop/admin');
+define("NO_IMAGE", ROOT . '/uploads/no_image.jpg');
+
+require_once ROOT . '/vendor/autoload.php';
